8a44c7c6e3c374e9474527b124cfa1ea8409fb7a
Nathaniel <semblancewarden@Gmail.com>
2021-12-01 23:43:26 +0800

    Merge remote-tracking branch 'origin/master'

--- Scripts/MiscScripts/Inventory Scripts/InventoryHold.cs
+++ Scripts/MiscScripts/Inventory Scripts/InventoryHold.cs
@@ -2,9 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Inventory))]
+[DisallowMultipleComponent]
 public class InventoryHold : MonoBehaviour
 {
-    InventoryScriptable inventoryScriptable;
+    Inventory inventory;
 
+    public Transform equipPosition;
 
+    GameObject held;
+
+    private void Start()
+    {
+        inventory = GetComponent<Inventory>();
+    }
+
+    private void Update()
+    {
+        SomeMethod();
+    }
+
+    public void SomeMethod()
+    {
+        if (inventory.GetInventoryLength() > 0)
+        {
+            if (held != null)
+            {
+                return;
+            }
+
+            held = Instantiate(inventory.GetInventory()[inventory.GetCurrentIndex()].GetMyself(), equipPosition.position, transform.rotation, equipPosition);
+        }
+    }
 }
